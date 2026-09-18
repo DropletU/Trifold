5ffc14de3541836577f945aaacb94ec3e1a63872
DropletU <dropletudi@gmail.com>
2026-09-18 16:19:30 +0300

    Reset Game
    Major changes planned

--- Scripts/Characters/States/PlayerStates/player_state.gd
+++
@@ -1,9 +0,0 @@
-@abstract class_name PlayerState extends RefCounted
-
-@abstract func enter()
-
-@abstract func exit()
-
-@abstract func _unhandled_input(player: Player, event: InputEvent)
-
-@abstract func _physics_process(player: Player, delta: float)
